21139f71200cd5993d7df898fbdd7f685c6e1dc8
chhavi6V <chhavigupta597@gmail.com>
2023-06-02 22:14:31 +0530

    we can fetch weather of any city across the world

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Ahmedabad&appid=41487a453cc27330060fc780d570d2b6&units=metric"
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req,res){
+    const query = req.body.cityName;
+    const apiKey = "41487a453cc27330060fc780d570d2b6"
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+ unit;
 
     https.get(url , function(response){
         console.log(response.statusCode);
@@ -16,13 +26,14 @@ app.get("/", function(req,res){
             const icon = WeatherData.weather[0].icon;
             const imageURL = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
             res.write("<p>The weather is currently "+ weatherDescription + "</p>");
-            res.write("<h1>The temp in Ahmedabad is "+ temp + " degree celcius</h1>");
+            res.write("<h1>The temp in "+ query + " is "+ temp + " degree celcius</h1>");
             res.write("<img src ="+ imageURL +">");
             res.send();
         })
     })
-
 })
+
+
 
 app.listen(3000, function(){
     console.log("server is running on port 3000");
